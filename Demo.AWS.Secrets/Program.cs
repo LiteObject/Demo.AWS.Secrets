8c238a0c1d19b8790a724980be1d941e68632a6d
LiteObject <Lite.Object@gmail.com>
2022-07-26 21:46:56 -0500

    Add project files.

---
+++ Demo.AWS.Secrets/Program.cs
@@ -0,0 +1,55 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Extensions.Caching;
+using Amazon.SecretsManager.Model;
+
+namespace Demo.AWS.Secrets
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var mySecretName = "MySecret";
+
+            var secret = await GetSecretFromCacheAsync(mySecretName);
+
+            if (string.IsNullOrWhiteSpace(secret))
+            {
+                secret = await GetSecretAsync(mySecretName);
+            }
+
+            Console.WriteLine("shhh!!! my secret: " + secret ?? "-- not found --");
+        }
+
+        private static async Task<string?> GetSecretAsync(string secretName)
+        {
+            var config = new AmazonSecretsManagerConfig { RegionEndpoint = RegionEndpoint.USEast2 };
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient("<AWS-ACCESS-KEY-ID>", "<AWS-SECRET-ACCESS-KEY>", config);
+
+            GetSecretValueRequest request = new GetSecretValueRequest()
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT" // VersionStage defaults to AWSCURRENT if unspecified.
+            };
+
+            GetSecretValueResponse? response = null;
+
+            try
+            {
+                response = await client.GetSecretValueAsync(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return response?.SecretString;
+        }
+
+        private static async Task<string> GetSecretFromCacheAsync(string secretName)
+        {
+            SecretsManagerCache cache = new();
+            return await cache.GetSecretString(secretName);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Links
+- [Retrieve AWS Secrets Manager secrets in .NET applications](https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_cache-net.html)
